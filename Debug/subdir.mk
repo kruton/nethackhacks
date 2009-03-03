18c57f4fe388eac8e30277ebeda93eb3d911bcf2
Kenny Root <kenny@the-b.org>
2009-03-03 11:12:12 -0600

    Add mysrandom.c to Debug profile

--- Debug/subdir.mk
+++ Debug/subdir.mk
@@ -3,17 +3,31 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../mysrandom.c 
+
 CPP_SRCS += \
 ../nhwisher.cpp 
 
 OBJS += \
+./mysrandom.o \
 ./nhwisher.o 
+
+C_DEPS += \
+./mysrandom.d 
 
 CPP_DEPS += \
 ./nhwisher.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
